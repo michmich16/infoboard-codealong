cac2e398f6a524109880ab2396c72b401628d461
Michael P <102651473+michmich16@users.noreply.github.com>
2024-04-04 14:01:53 +0200

    menu

--- assets/js/Components/Menu.js
+++ assets/js/Components/Menu.js
@@ -3,6 +3,27 @@ import { myFetch } from "../Utils/apiUtils.js"
 export const Menu = async () => {
     console.log("kald af menu funktion");
     const endpoint = "https://infoskaerm.techcollege.dk/umbraco/api/content/getcanteenmenu/?type=json";
-    const data = myFetch(endpoint)
-    console.log(data);
+    const data = await myFetch(endpoint)
+
+    const container = document.getElementById("menu")
+    container.innerHTML = ""
+
+    const curday = (new Date().getDay()-1)
+
+    const ul = document.createElement("ul")
+
+    data && data.Days && data.Days.map((value, key) => {
+        const li = document.createElement("li")
+        li.innerHTML = `<span>${value.DayName}:</span>
+                        <span>${value.Dish}:</span>`
+        ul.appendChild(li)
+    })
+
+    container.appendChild(ul)
+
+    setInterval(() => {
+        Menu()
+    }, 3600000)
+
+
 }
